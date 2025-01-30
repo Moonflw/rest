5e1abf7baaf792caf53bcbe3b03b6bca13f96d83
Urdabaev99 <ulugbek9324431@gmail.com>
2025-01-30 17:51:16 +0500

    add about desc and mob

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,52 @@
 // import Swiper from "swiper";
 import { plugins } from "./components/plugins.js";
-import { lenis } from "./components/lenis.js"
+import { lenis } from "./components/lenis.js";
 import { intro } from "./sections/intro.js";
+import { invest } from "./sections/invest.js";
+import { developer } from "./sections/developer.js";
+import { location } from "./sections/location.js";
+
+
+
+
+
+import { about } from "./sections/about.js";
 import { validationPhoneInput } from "./components/validation-phone-input.js";
 
 window.addEventListener("onbeforeunload", function () {
-    window.scrollTo(0, 0);
-    gsap.to(window, { duration: 0, scrollTo: 0 });
+  window.scrollTo(0, 0);
+  gsap.to(window, { duration: 0, scrollTo: 0 });
 });
 window.addEventListener("unload", function () {
-    window.scrollTo(0, 0);
-    gsap.to(window, { duration: 0, scrollTo: 0 });
+  window.scrollTo(0, 0);
+  gsap.to(window, { duration: 0, scrollTo: 0 });
 });
 
-
 window.addEventListener("load", () => {
-    plugins()
-    lenis()
-    // Swiper.use([Navigation, Autoplay])
+  plugins();
+  lenis();
+  // Swiper.use([Navigation, Autoplay])
 
-    validationPhoneInput()
-    intro()
-    invest()
-    developer()
-    location()
+  validationPhoneInput();
+  intro();
+  about();
+  invest();
+  developer();
+  location();
 
-    // Zoom Site Disabled
-    document.addEventListener('gesturestart', function (e) {
-        e.preventDefault();
-        document.body.style.zoom = 1;
-    });
+  // Zoom Site Disabled
+  document.addEventListener("gesturestart", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 1;
+  });
 
-    document.addEventListener('gesturechange', function (e) {
-        e.preventDefault();
-        document.body.style.zoom = 1;
-    });
+  document.addEventListener("gesturechange", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 1;
+  });
 
-    document.addEventListener('gestureend', function (e) {
-        e.preventDefault();
-        document.body.style.zoom = 1;
-    });
-})
+  document.addEventListener("gestureend", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 1;
+  });
+});
--- src/js/sections/about.js
+++ src/js/sections/about.js
@@ -1,1 +1,14 @@
-export default function about() {}
+import { queryMatches } from "../components/utils.js";
+export function about() {
+  const mob = queryMatches(768, "max");
+  if (mob) {
+    var swiper = new Swiper(".about-swiper", {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+      pagination: {
+        // el: ".swiper-pagination",
+        // clickable: true,
+      },
+    });
+  }
+}
