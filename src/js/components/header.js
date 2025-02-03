280fc4d55f94d86d88292b84a7184fc9210f88c8
Urdabaev99 <ulugbek9324431@gmail.com>
2025-02-03 10:52:27 +0500

    fix:header

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,8 +1,8 @@
 export function header() {
   const headerBtnMenu = document.querySelector(".header__menu-btn");
   const header = document.querySelector("header");
-  const headerSocial = document.querySelectorAll(".header__social .social");
-  const headerSocialBtn = document.querySelector(".header-social-btn");
+  // const headerSocial = document.querySelectorAll(".header__social .social");
+  // const headerSocialBtn = document.querySelector(".header-social-btn");
   headerBtnMenu.addEventListener("click", () => {
     header.classList.toggle("open-menu");
   });
@@ -14,17 +14,17 @@ export function header() {
       end: "bottom-=50 top",
       onEnter: () => {
         header.classList.add("header-dark");
-        headerSocial.forEach((el) => el.classList.remove("social-white"));
-        headerSocial.forEach((el) => el.classList.add("social-dark"));
-        headerSocialBtn.classList.add("dark");
-        headerSocialBtn.classList.remove("white");
+        // headerSocial.forEach((el) => el.classList.remove("social-white"));
+        // headerSocial.forEach((el) => el.classList.add("social-dark"));
+        // headerSocialBtn.classList.add("dark");
+        // headerSocialBtn.classList.remove("white");
       },
       onEnterBack: () => {
         header.classList.remove("header-dark");
-        headerSocial.forEach((el) => el.classList.add("social-white"));
-        headerSocial.forEach((el) => el.classList.remove("social-dark"));
-        headerSocialBtn.classList.remove("dark");
-        headerSocialBtn.classList.add("white");
+        // headerSocial.forEach((el) => el.classList.add("social-white"));
+        // headerSocial.forEach((el) => el.classList.remove("social-dark"));
+        // headerSocialBtn.classList.remove("dark");
+        // headerSocialBtn.classList.add("white");
       },
     },
   });
