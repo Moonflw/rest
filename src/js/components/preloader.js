3dc1891958cd41ed6699a20847a4c7d9067c00fb
Urdabaev99 <ulugbek9324431@gmail.com>
2025-02-05 17:50:03 +0500

    fix:time preloader

--- src/js/components/preloader.js
+++ src/js/components/preloader.js
@@ -5,12 +5,14 @@ export function preloader() {
     clipPath: "inset(0 0% 0 0)",
     duration: 2,
     ease: "none",
-    onComplete: () => {
-      document.body.classList.remove("no-scroll");
-      document.body.classList.add("loaded");
+    onStart: () => {
       document.querySelectorAll(".iti__country-list").forEach((el) => {
         el.style.cssText = "opacity:0;visabilit:hidden";
       });
+    },
+    onComplete: () => {
+      document.body.classList.remove("no-scroll");
+      document.body.classList.add("loaded");
       setTimeout(() => {
         document.querySelectorAll(".iti__country-list").forEach((el) => {
           el.style.cssText = "opacity:1;visabilit:visible";
