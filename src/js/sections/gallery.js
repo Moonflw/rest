90b343a4e6dd22eded65f7d1bcf703e075bc448d
Munir Sasfin <followmoon98@gmail.com>
2025-07-18 19:05:37 +0500

    theSecond

--- src/js/sections/gallery.js
+++ src/js/sections/gallery.js
@@ -1,19 +1,35 @@
 export function gallery() {
     
  const gallery = document.querySelector('.gallery');
+ const items = document.querySelectorAll('.item');
   const itemTl = gsap.timeline({
     scrollTrigger: {
       trigger: gallery,
       start: 'top center',
       end: 'bottom bottom',
-    //   markers: true,
+      markers: true,
       once: true,
       onEnter: () => {
         gallery.classList.add('show');
       },
       
     },
-    
 });
-console.log("asdas");
+items.forEach((item) => {
+     const itemT = gsap.timeline({
+        scrollTrigger: {
+          trigger: item,
+          start: 'top center',
+          end: 'bottom bottom',
+          markers: true,
+          once: true,
+          onEnter: () => {
+            item.classList.add('show');
+            console.log('====================================');
+            console.log("dada");
+            console.log('====================================');
+          },
+        }
+      });
+    });
 }
